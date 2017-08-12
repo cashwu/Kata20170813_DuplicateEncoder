5e0f00b71f0b6508ec3a50bcf720f154d8e8efa5
cash <cash@cashwu.com>
2017-08-12 23:06:30 +0800

    init

--- Kata20170813_DuplicateEncoder/DuplicateEncoderTests.cs
+++ Kata20170813_DuplicateEncoder/DuplicateEncoderTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170813_DuplicateEncoder
 {
     [TestClass]
-    public class UnitTest1
+    public class DuplicateEncoderTests
     {
         [TestMethod]
         public void TestMethod1()
